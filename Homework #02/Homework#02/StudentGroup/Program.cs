04ddf1618735d4267951a5e7d4b2adb0fef8d1aa
BojanPavlovski <119736981+BojanPavlovski@users.noreply.github.com>
2023-03-08 21:20:55 +0100

    Add files via upload

---
+++ Homework #02/Homework#02/StudentGroup/Program.cs
@@ -0,0 +1,26 @@
+﻿//Task 2
+string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[] { "Bojan", "Stojan", "Marko", "Sharko", "Maja" };
+
+Console.WriteLine("Enter 1 or 2");
+string input = Console.ReadLine();
+
+if (input == "1")
+{
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (input == "2")
+{
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Enter a valid input");
+}
+
---
+++ Homework #02/Homework#02/SumOfEven/Program.cs
@@ -0,0 +1,33 @@
+﻿//Task 1
+int[] arrayOfNumbers = new int[6];
+int sum = 0;
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine("Enter a digit:");
+    arrayOfNumbers[i] = int.Parse(Console.ReadLine());
+
+}
+
+
+foreach (int number in arrayOfNumbers)
+{
+
+    if (number % 2 == 0)
+    {
+        sum += number;
+
+    }
+    else if (number % 2 == 1)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+
+
+}
+Console.WriteLine($"The result is {sum}");
